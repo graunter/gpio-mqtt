382223115ab8db71c5fbc77919b5dd2231a2e392
graunter <graunter@gmail.com>
2025-02-07 11:43:09 +0000

    an installation issue was resolve

--- project/scripts/package/etc/gpio-mqtt/start.sh
+++ project/scripts/package/etc/gpio-mqtt/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -d /var/log/gpio-mqtt ]; then
+        mkdir /var/log/gpio-mqtt
+        echo "Folder for logs was created"
+fi
+
 /opt/gpio-mqtt/gpio-mqtt -v &>> /var/log/gpio-mqtt/gpio-mqtt.log & #> /dev/null 2>&1
 
 echo $! > /var/run/gpio-mqtt.pid
